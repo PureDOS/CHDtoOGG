7158586dc5c73c230f9c7c6b52f033ca3cb62877
PureDOS <136184507+PureDOS@users.noreply.github.com>
2025-02-06 22:35:37 +0900

    Rename file

--- build-clang.sh
+++ build-clang.sh
@@ -1,3 +1,3 @@
 echo Building \'CHDtoOGG\' ...
-clang++ -std=c++11 -O3 -Wall CHDtoOGG.cpp EncodeVorbis.wasm.cpp -o CHDtoOGG
+clang++ -std=c++11 -O3 -Wall CHDtoOGG.cpp WasmEncodeVorbis.cpp -o CHDtoOGG
 echo Done!
--- build-gcc.sh
+++ build-gcc.sh
@@ -1,3 +1,3 @@
 echo Building \'CHDtoOGG\' ...
-g++ -std=c++11 -O3 -Wall CHDtoOGG.cpp EncodeVorbis.wasm.cpp -o CHDtoOGG
+g++ -std=c++11 -O3 -Wall CHDtoOGG.cpp WasmEncodeVorbis.cpp -o CHDtoOGG
 echo Done!
